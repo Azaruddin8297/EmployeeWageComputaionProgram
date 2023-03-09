36977f43f35990704c0778fd61cae12d0c18bae3
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-03-09 11:09:35 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeWageComputationProblem
+{
+    public class Grettings
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome tho the Employee wage computation program");
+            EmployeeCheck_UC1.EmployeeCheck();
+        }
+    }
+
+}
