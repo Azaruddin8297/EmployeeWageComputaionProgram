141467e9e7445824dc773a104e26473d12f30f97
Azaruddin8297 <ajarpopo.com>
2023-03-09 12:01:00 +0530

    New wages

--- EmployeeCheck_UC1.cs
+++ EmployeeCheck_UC1.cs
@@ -11,16 +11,26 @@ namespace EmployeeWageComputationProblem
         public static void EmployeeCheck()
         {
             int IS_FULL_TIME = 1;
+            int emprateperhr = 20;
+            int emphrs = 0;
+            int empwages = 0;
+
+
             Random random = new Random();
             int empCheck = random.Next(2);
             if(empCheck == 1)
             {
                 Console.WriteLine("Employee is presrent");
+                emphrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is absent");
+                emphrs = 0;
             }
+            empwages = emphrs * emprateperhr;
+
+            Console.WriteLine("Total wages of employye is " + empwages);
         }
     }
 }
