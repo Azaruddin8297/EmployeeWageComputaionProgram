5bb4f9221b6faacc75c3a258e08921ab64484335
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-03-09 10:52:47 +0530

    Employee wage

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWageComputationProblem
+{
+    public class Grettings
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome tho the Employee wage computation program");
+        }
+    }
+
+}
