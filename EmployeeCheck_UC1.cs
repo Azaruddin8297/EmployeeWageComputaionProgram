c4f36ea07d0d68f9ba602967833192d14bc2685e
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-03-10 22:01:53 +0530

    switch case

--- EmployeeCheck_UC1.cs
+++ EmployeeCheck_UC1.cs
@@ -12,25 +12,31 @@ namespace EmployeeWageComputationProblem
         {
             int IS_FULL_TIME = 1;
             int emprateperhr = 20;
+            int IS_PART_TIME = 2;
             int emphrs = 0;
             int empwages = 0;
 
 
             Random random = new Random();
-            int empCheck = random.Next(2);
-            if(empCheck == 1)
-            {
-                Console.WriteLine("Employee is presrent");
-                emphrs = 8;
-            }
-            else
-            {
-                Console.WriteLine("Employee is absent");
-                emphrs = 0;
+            int empCheck = random.Next(3);
+            switch (empCheck) {
+                case 0:
+                    Console.WriteLine("Employee is presrent");
+                    emphrs = 8;
+                    break;
+                case 1:
+                    Console.WriteLine("Employee is a part Timer");
+                    emphrs = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is absent");
+                    emphrs = 0;
+                    break;
             }
             empwages = emphrs * emprateperhr;
 
             Console.WriteLine("Total wages of employye is " + empwages);
+
         }
     }
 }
